6859cebb56bb150a91164ba578c2304c12a4d66f
alichoumane <ali.choumane@gmail.com>
2023-12-09 11:53:20 +0200

    init commit

---
+++ lib/home.dart
@@ -0,0 +1,82 @@
+import 'package:flutter/material.dart';
+import 'product.dart';
+import 'search.dart';
+
+class Home extends StatefulWidget {
+  const Home({super.key});
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  bool _loaded = false; // used to show products list or progress bar
+
+  void update(bool success) {
+    setState(() {
+      _loaded = true; // show product list
+      if (!success) { // API request failed
+        ScaffoldMessenger.of(context).showSnackBar(const SnackBar(content: Text('Failed to load data')));
+      }
+    });
+  }
+
+  @override
+  void initState() {
+    // update data when the widget is added to the tree the first tome.
+    getProducts(update);
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(actions: [
+          IconButton(onPressed: !_loaded ? null : () {
+            setState(() {
+              _loaded = false; // show progress bar
+              getProducts(update); // update data asynchronously
+            });
+          }, icon: const Icon(Icons.refresh)),
+          IconButton(onPressed: () {
+            setState(() { // open the search product page
+               Navigator.of(context).push(
+                   MaterialPageRoute(builder: (context) => const Search())
+               );
+            });
+          }, icon: const Icon(Icons.search))
+        ],
+          title: const Text('Available Products'),
+          centerTitle: true,
+        ),
+        // load products or progress bar
+        body: _loaded ? const ShowProducts() : const Center(
+            child: SizedBox(width: 100, height: 100, child: CircularProgressIndicator())
+        )
+    );
+  }
+}
+
+class ShowProducts extends StatelessWidget {
+  const ShowProducts({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    double width = MediaQuery.of(context).size.width;
+    return ListView.builder(
+        itemCount: products.length,
+        itemBuilder: (context, index) {
+          return Column(children: [
+            const SizedBox(height: 10.0),
+            Row(children: [
+              SizedBox(width: width * 0.15,),
+              Text(products[index].toString(), style: const TextStyle(fontSize: 18)),
+              SizedBox(width: width*0.15,),
+
+            ]),
+            const SizedBox(height: 10.0)
+          ]);
+        });
+  }
+}
+
---
+++ lib/product.dart
@@ -0,0 +1,85 @@
+import 'dart:convert' as convert;
+import 'package:http/http.dart' as http;
+
+String _baseURL = 'mobileappdemo.000webhostapp.com';
+// class to represent a row from the products table
+// note: cid is replaced by category name
+class Product {
+  int _pid;
+  String _name;
+  int _quantity;
+  double _price;
+  String _category;
+
+  Product(this._pid, this._name, this._quantity, this._price, this._category);
+
+  @override
+  String toString() {
+    return """
+      PID: $_pid Name: $_name 
+      Quantity: $_quantity 
+      Price: \$$_price 
+      Category: $_category """;
+  }
+}
+
+// list to hold products retrieved from getProducts
+List<Product> products = [];
+
+// asynchronously update _products list
+void getProducts(Function(bool success) update) async {
+  try {
+    final url = Uri.https(_baseURL, 'getProducts.php');
+    final response = await http.get(url)
+        .timeout(const Duration(seconds: 5)); // max timeout 5 seconds
+    products.clear(); // clear old products
+    if (response.statusCode == 200) { // if successful call
+      final jsonResponse = convert.jsonDecode(response.body); // create dart json object from json array
+      for (var row in jsonResponse) { // iterate over all rows in the json array
+        Product p = Product( // create a product object from JSON row object
+            int.parse(row['pid']),
+            row['name'],
+            int.parse(row['quantity']),
+            double.parse(row['price']),
+            row['category']);
+        products.add(p); // add the product object to the _products list
+      }
+      update(true); // callback update method to inform that we completed retrieving data
+    }
+  }
+  catch(e) {
+    print('Error loading data with error ${e.toString()}');
+    update(false); // inform through callback that we failed to get data
+  }
+}
+
+
+// asynchronously update _products list
+void getProductById(Function(String text) update, int pid) async {
+  try {
+    final url = Uri.https(_baseURL, 'getProduct_By_ID.php', {'pid':'$pid'});
+    final response = await http.get(url)
+        .timeout(const Duration(seconds: 5)); // max timeout 5 seconds
+    if (response.statusCode == 200) { // if successful call
+      final jsonResponse = convert.jsonDecode(response.body); // create dart json object from json array
+      if(jsonResponse.length == 0)
+        update("No product found");
+      else{
+        for (var row in jsonResponse) { // iterate over all rows in the json array, there should be at most one product as pid is a primary key
+          Product p = Product( // create a product object from JSON row object
+              int.parse(row['pid']),
+              row['name'],
+              int.parse(row['quantity']),
+              double.parse(row['price']),
+              row['category']);
+          update(p.toString()); // callback update method to inform that we completed retrieving data
+          break;
+        }
+      }
+    }
+  }
+  catch(e) {
+    print('Error loading data with error ${e.toString()}');
+    update("Can't load data"); // inform through callback that we failed to get data
+  }
+}
